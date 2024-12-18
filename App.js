a92e1b6b37773c0cfcdd3c82d5088578bb2316a2
schoolscaler <codea2zwithsujeet@gmail.com>
2024-12-18 15:31:48 +0530

    Episode 3 code

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from react!"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+//JSX
+const Title = () => (<h1 id="heading">Namaste React using JSX 🚀</h1>);
+
+//React Component -
+//Class based  component - OLD
+//Functional based component - NEW
+
+const HeadingComponent = () => (
+  <div id="container">
+    {1000}
+    <Title />
+    <h1>Namaste React from Function component</h1>
+  </div> 
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # namaste-react-tutorial
+
+# Parcel
+- Dev Build
+- local server
+- HMR - Hot Module Replacement
+- File watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling 
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- Tree Shaking - remove umused code 
